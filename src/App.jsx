e0f94f728c06e59f0315daf114c8e5a8fc1366bc
Sarthak Chugh <sarthak@Sarthaks-Mac-mini.local>
2024-12-01 17:49:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <div>This is my application.</div>
+        </>
+    );
+}
+
+export default App;
